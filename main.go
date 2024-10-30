ac5efc2eec3a1889af2bcaf6ad68f4767d318037
Karuna053 <karunayauw@gmail.com>
2024-10-30 21:53:21 +0700

    Finishing the barebone beginning.

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "rest-api/routers"
 
+func main() {
+	routers.StartRouter().Run(":8080")
 }
--- routers/routes.go
+++ routers/routes.go
@@ -1,5 +1,15 @@
 package routers
 
 import (
-	_ "rest-api/docs"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+func StartRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	return router
+}
